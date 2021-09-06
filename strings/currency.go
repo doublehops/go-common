df19fb5990bbd4f3c5e3422a643cd67cb49a5b52
Damien Buttler <damien@doublehops.com>
2021-09-06 15:44:44 +1000

    Added currency formatter.

--- strings/currency.go
+++ strings/currency.go
@@ -5,15 +5,30 @@ import (
 	"strconv"
 )
 
-func AddSeparator(value float64) string {
+func AddSeparator(value float64, separator string) string {
 	i := int(value)
 	d := fmt.Sprintf("%.2g", value-float64(i))[2:]
 
-	val := separate(strconv.Itoa(i), ",")
+	//val := separate(strconv.Itoa(i), ",")
+	str := strconv.Itoa(i)
 
-	v := fmt.Sprintf("%s.%s", val, d)
+	var newValue string
+	for {
+		if len(str) > 3 {
+			newValue = separator + str[len(str)-3:] + newValue
+			str = str[:len(str)-3]
+		} else {
+			return str + newValue
+		}
+	}
+
+	v := fmt.Sprintf("%s.%s", newValue, d)
 
 	return v
+
+	//v := fmt.Sprintf("%s.%s", val, d)
+	//
+	//return v
 }
 
 func separate(str string, separator string) string {
