01d1869fd6ec3dab45a59353cc7773d6a4a7bb33
Damien Buttler <damien@doublehops.com>
2021-09-20 12:00:40 +1000

    Better methods to convert to string.

--- strings/currency.go
+++ strings/currency.go
@@ -2,45 +2,44 @@ package strings
 
 import (
 	"fmt"
-	"strconv"
+	"math"
+	"strings"
 )
 
-func AddSeparator(value float64, separator string) string {
-	i := int(value)
-	d := fmt.Sprintf("%.2g", value-float64(i))[2:]
+// ConvertToCurrency will convert a float into human-readable currency string. The delimiters are
+// optional but by will default will convert 123567.89 to format of 123,456.69
+func ConvertToCurrency(value float64, delimiters ...string) string {
+	var dollarDelimiter string = ","
+	var centDelimiter string = "."
+	if len(delimiters) > 0 {
+		dollarDelimiter = delimiters[0]
+	}
+	if len(delimiters) == 2 {
+		centDelimiter = delimiters[1]
+	}
 
-	//val := separate(strconv.Itoa(i), ",")
-	str := strconv.Itoa(i)
+	d, c := math.Modf(value)
+	dollars := fmt.Sprintf("%f", d)
+	cents := fmt.Sprintf("%f", c)
+	dollars = dollars[0:strings.Index(dollars, ".")] // get string before `.`
+	cents = cents[strings.Index(cents, ".")+1:4] // get string after `.`
+	str := dollars
 
 	var newValue string
 	for {
 		if len(str) > 3 {
-			newValue = separator + str[len(str)-3:] + newValue
+			newValue = str[len(str)-3:] + newValue
 			str = str[:len(str)-3]
+			if len(str) > 0 {
+				newValue = dollarDelimiter + newValue
+			}
 		} else {
-			return str + newValue
+			newValue = str + newValue
+			break
 		}
 	}
 
-	v := fmt.Sprintf("%s.%s", newValue, d)
+	v := fmt.Sprintf("%s%s%s", newValue, centDelimiter, cents)
 
 	return v
-
-	//v := fmt.Sprintf("%s.%s", val, d)
-	//
-	//return v
-}
-
-func separate(str string, separator string) string {
-	var newValue string
-	for {
-		if len(str) > 3 {
-			newValue = separator + str[len(str)-3:] + newValue
-			str = str[:len(str)-3]
-		} else {
-			return str + newValue
-		}
-	}
-
-	return newValue
 }
--- strings/currency_test.go
+++ strings/currency_test.go
@@ -3,24 +3,43 @@ package strings
 import "testing"
 
 func TestAddSeparator(t *testing.T) {
-	var value float64
-	value = 1234567.89
+	var tests = []struct{
+		Name string
+		Value float64
+		Expected string
+	}{
+		{"hundreds", 134.81122, "134.81"},
+		{"thousands", 1234.8012, "1,234.80"},
+		{"millions", 12345678, "12,345,678.00"},
+	}
 
-	//type subTestStruct struct{
-	//	Name string
-	//	Value float64
-	//	Expected string
-	//}
-	//
-	//subTests := []subTestStruct{
-	//
-	//
-	//}
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			received := ConvertToCurrency(test.Value)
+			if received != test.Expected {
+				t.Errorf("expected: %s; received: %s", test.Expected, received)
+			}
+		})
+	}
+}
 
-	expected := "1,234,567.89"
-	foundVal := AddSeparator(value)
+func TestAddEuropeanSeparator(t *testing.T) {
+	var tests = []struct{
+		Name string
+		Value float64
+		Expected string
+	}{
+		{"hundreds", 134.81, "134,81"},
+		{"thousands", 1234.8012, "1.234,80"},
+		{"millions", 12345678.80, "12.345.678,80"},
+	}
 
-	if foundVal != expected {
-		t.Fatalf("Value incorrect. Expected: %s; found: %s", expected, foundVal)
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			received := ConvertToCurrency(test.Value, ".", ",")
+			if received != test.Expected {
+				t.Errorf("expected: %s; received: %s", test.Expected, received)
+			}
+		})
 	}
 }
